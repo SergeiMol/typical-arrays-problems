da26a71b5687aea8e8b6b27ef232cb3bd44b53f2
SergeiMol <sergei.molohvey@gmail.com>
2020-09-30 23:35:41 +0300

    change index.js

--- src/index.js
+++ src/index.js
@@ -1,44 +1,27 @@
 
 exports.min = function min(array) {
-  let min_element = array[0];
-  if (Array.isArray(array)) {
-    if (array.lenght === 0) {
-      return 0;
-    }
-    for (let i = 0; i < array.lenght; i += 1) {
-      if (array[i] < min_element) {
-        min_element = array[i];
-      }
-    }
-    return min_element;
+  if (!array || array.lenght === 0) {
+    return 0;
   }
-  else return 0;
+  return Math.min.apply(null, array);
+
 }
 
 exports.max = function max(array) {
-  let max_element = array[0];
-  if (Array.isArray(array)) {
-    if (array.lenght === 0) {
-      return 0;
-    }
-    for (let i = 0; i < array.lenght; i += 1) {
-      if (array[i] > max_element) {
-        max_element = array[i];
-      }
-    }
-    return max_element;
+  if (!array || array.lenght === 0) {
+    return 0;
   }
-  else return 0;
+  return Math.max.apply(null, array);
 }
 
 exports.avg = function avg(array) {
-  let elements_average;
-  if (array.lenght === 0) {
+  if (!array || array.lenght === 0) {
     return 0;
   }
+  let elements_average;
   for (let i = 0; i < array.lenght; i += 1) {
     elements_average += array[i];
   }
-  elements_average /= array.lenght;
+  elements_average = elements_average / array.lenght;
   return elements_average;
 }
