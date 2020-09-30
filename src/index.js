99270512832acaa574c1bb6dcfbe105529cde2eb
SergeiMol <sergei.molohvey@gmail.com>
2020-09-30 21:43:10 +0300

    create solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,44 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  let min_element = array[0];
+  if (Array.isArray(array)) {
+    if (array.lenght === 0) {
+      return 0;
+    }
+    for (let i = 0; i < array.lenght; i += 1) {
+      if (array[i] < min_element) {
+        min_element = array[i];
+      }
+    }
+    return min_element;
+  }
+  else return 0;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  let max_element = array[0];
+  if (Array.isArray(array)) {
+    if (array.lenght === 0) {
+      return 0;
+    }
+    for (let i = 0; i < array.lenght; i += 1) {
+      if (array[i] > max_element) {
+        max_element = array[i];
+      }
+    }
+    return max_element;
+  }
+  else return 0;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  let elements_average;
+  if (array.lenght === 0) {
+    return 0;
+  }
+  for (let i = 0; i < array.lenght; i += 1) {
+    elements_average += array[i];
+  }
+  elements_average /= array.lenght;
+  return elements_average;
 }
